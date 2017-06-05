bf2a2e241e5a0140214422a578a382687f48d1c8
GingiWang <pokitimmy@hotmail.com>
2017-06-05 14:24:14 +0800

    123

---
+++ FoodMap/Areas/FoodMapArea/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FoodMap.Areas.FoodMapArea.Models;
+
+namespace FoodMap.Areas.FoodMapArea.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private superuniversityEntities db = new superuniversityEntities();
+        // GET: FoodMapArea/Customer
+        public ActionResult Index()
+        {
+            
+            return View(db.Shop.Find(1));
+        }
+    }
+}
